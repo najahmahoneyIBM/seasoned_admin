8312f0b2814736de89d428377c1cf6f24c885925
najahmahoneyIBM <najah.mahoney@ibm.com>
2022-04-08 08:31:57 -0500

    Removed build folderfrom ignore file

---
+++ build/precache-manifest.13ba3064e05eafb46b73e2b5d599cf92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de64fcaccfd1d13e687e177ebb9dbe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdaade8ffce5d718e51c",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0deee3f87ace9c5eac52",
+    "url": "/static/css/main.094f470e.chunk.css"
+  },
+  {
+    "revision": "cdaade8ffce5d718e51c",
+    "url": "/static/js/2.a9947fc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a9947fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0deee3f87ace9c5eac52",
+    "url": "/static/js/main.40269dc8.chunk.js"
+  },
+  {
+    "revision": "b1e771b7abaad0775a5e",
+    "url": "/static/js/runtime-main.9c4303a4.js"
+  }
+]);
